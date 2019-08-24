a191be904e4e478248d04bcbef522e0b2c5c8fa6
vikram verma <vikram.verma@careongo.com>
2019-08-24 14:50:11 +0530

    menu pages for different types

--- django_tut/restaurant/webpages/urls.py
+++ django_tut/restaurant/webpages/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('home', views.index, name='index'),
 	# url(r'^$', views.index, name='index'),
     path('contact-us', views.contact, name='contact-us'),
+    path('continental', views.continental, name='continental'),
+    path('indian', views.indian, name='indian'),
     path('menu', views.menu, name='menu')
 ]
--- django_tut/restaurant/webpages/views.py
+++ django_tut/restaurant/webpages/views.py
@@ -8,6 +8,19 @@ def contact(request):
     phone_no = "0120-1234567"
     return render(request=request, template_name='contact-us.html', context={'phone_no':phone_no})
 
-def menu(request):
-    menu=["sandwitches", "pasta"]
+def continental(request):
+    menu=["sandwitches", "pasta", "burgers"]
     return render(request=request, template_name='menu.html', context={'menu_items':menu})
+
+def indian(request):
+    menu = ["dal roti", "chole bhatuure", "aalu puri"]
+    return render(request=request, template_name='menu.html', context={'menu_items':menu})
+
+def menu(request):
+    if request.GET['type'] == "indian":
+        menu = ["dal roti", "chole bhatuure", "aalu puri"]
+    elif request.GET['type'] == "continental":
+        menu = ["sandwitches", "pasta", "burgers"]
+    else:
+        menu = []
+    return render(request=request, template_name='menu.html', context={'menu_items': menu})
