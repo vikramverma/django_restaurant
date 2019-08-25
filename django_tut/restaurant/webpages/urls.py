03f0054ab91b6a33db61c86523a5f56bcb74f710
vikram verma <vikram.verma@careongo.com>
2019-08-25 14:46:27 +0530

    get parameter changed to link and url names

--- django_tut/restaurant/webpages/urls.py
+++ django_tut/restaurant/webpages/urls.py
@@ -2,11 +2,17 @@ from django.urls import path
 from django.conf.urls import url
 from . import views
 
+app_name = "website"
+
 urlpatterns = [
+    # url(r'^dishes/(?P<dishes_type>[a-zA-Z]+)', views.menu, name='menu'),
     path('home', views.index, name='index'),
-	# url(r'^$', views.index, name='index'),
-    path('contact-us', views.contact, name='contact-us'),
-    path('continental', views.continental, name='continental'),
-    path('indian', views.indian, name='indian'),
-    path('menu', views.menu, name='menu')
+	path('contact-us', views.contact, name='contact-us'),
+    # path('continental', views.continental, name='continental'),
+    # path('indian', views.indian, name='indian'),
+    # path('dishes/continental', views.menu, name='continental_menu'),
+    # path('menu/indian', views.menu_indian, name='indian_menu'),
+    # path('menu/chinese', views.menu_chinese, name='chinese_menu'),
+    url(r'menu/(?P<type>[a-zA-Z]+)', views.menu, name='menu')
+    # path('dishes/indian', views.menu, name='indian_menu')
 ]
--- django_tut/restaurant/webpages/views.py
+++ django_tut/restaurant/webpages/views.py
@@ -8,19 +8,26 @@ def contact(request):
     phone_no = "0120-1234567"
     return render(request=request, template_name='contact-us.html', context={'phone_no':phone_no})
 
-def continental(request):
-    menu=["sandwitches", "pasta", "burgers"]
-    return render(request=request, template_name='menu.html', context={'menu_items':menu})
+# def continental(request):
+#     menu=["sandwitches", "pasta", "burgers"]
+#     return render(request=request, template_name='menu.html', context={'menu_items':menu})
 
-def indian(request):
-    menu = ["dal roti", "chole bhatuure", "aalu puri"]
-    return render(request=request, template_name='menu.html', context={'menu_items':menu})
+# def menu_indian(request):
+#     menu = ["dal roti", "chole bhatuure", "aalu puri"]
+#     return render(request=request, template_name='menu.html', context={'menu_items':menu})
+#
+# def menu_chinese(request):
+#     menu = ["noodles"]
+#     return render(request=request, template_name='menu.html', context={'menu_items': menu})
 
-def menu(request):
-    if request.GET['type'] == "indian":
+def menu(request, type):
+    # type = request.GET['type']
+    if type == "indian":
         menu = ["dal roti", "chole bhatuure", "aalu puri"]
-    elif request.GET['type'] == "continental":
+    elif type == "continental":
         menu = ["sandwitches", "pasta", "burgers"]
+    elif type == "chinese":
+        menu = ["noodles"]
     else:
         menu = []
     return render(request=request, template_name='menu.html', context={'menu_items': menu})
