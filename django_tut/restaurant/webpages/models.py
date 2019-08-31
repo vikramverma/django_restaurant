705103a7211d2b638ed8c718a1fb2b54d7c691a9
vikram verma <vikram.verma@careongo.com>
2019-08-31 18:20:45 +0530

    creating tables for db

--- django_tut/restaurant/webpages/models.py
+++ django_tut/restaurant/webpages/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+import datetime
 # Create your models here.
+
+class Dish(models.Model):
+    name = models.CharField(max_length=50)
+    rate = models.FloatField()
+    isVeg = models.BooleanField(default=True)
+    category = models.CharField(max_length=50)
+    image = models.TextField()
+    available_from = models.DateTimeField(default=datetime.datetime.now())
+    is_available = models.BooleanField()
+
+
+class Discount(models.Model):
+    discount_value = models.FloatField()
+    discount_on = models.ForeignKey(Dish, on_delete=models.CASCADE)
+
+
+class PromoCode(models.Model):
+    code = models.TextField()
+    valid_till = models.DateField()
